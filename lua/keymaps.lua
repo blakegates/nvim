9f55fa3b9a87731645595535e8cfd0bb8ae1251e
blakegates <blakegatesllc@gmail.com>
2025-01-27 18:32:34 -0500

    keymap and options changes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,42 +1,42 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+--  See `:help lua-guide-autocommands`
+--  See `:help wincmd` for a list of all window commands
 
 -- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Copy to clipboard keymaps
+vim.keymap.set('v', '<C-S-c>', '"+y', { desc = 'Copy selected text to clipboard' })
+vim.keymap.set('n', '<C-S-c>', '"+y', { desc = 'Copy current line to clipboard' })
+
+-- Paste from clipboard keymaps
+vim.keymap.set('n', '<C-S-v>', '"+p', { desc = 'Paste from clipboard' })
+vim.keymap.set('n', '<C-S-V>', '"+P', { desc = 'Paste from clipboard (before cursor)' })
+
+-- Cut to clipboard keymaps
+vim.keymap.set('n', '<C-S-x>', '"+d', { desc = 'Cut current line to clipboard' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
+-- Terminal Mode keymaps
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('n', '<C-\\>', ':Floaterminal<CR>', { desc = 'Open floaterminal' })
--- TIP: Disable arrow keys in normal mode
+
+-- Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
+-- Split window navigation keymaps
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
 -- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
